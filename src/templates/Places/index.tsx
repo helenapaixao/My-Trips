0e18b4c0c71071b6f04b63a9a6563b87a2c3c3e7
helenapaixaopixter <helena.paixao@pixter.com.br>
2021-12-22 13:56:34 -0300

    feat: config component next image

--- src/templates/Places/index.tsx
+++ src/templates/Places/index.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
+import { useRouter } from 'next/dist/client/router'
 import LinkWrapper from 'components/LinkWrapper'
 import { CloseOutline } from '@styled-icons/evaicons-outline'
+import * as S from './styles'
 
 type ImageProps = {
   url: string
@@ -12,7 +14,7 @@ export type PlacesTemplateProps = {
   place: {
     slug: string
     name: string
-    description: {
+    description?: {
       html: string
     }
     gallery: ImageProps[]
@@ -20,23 +22,39 @@ export type PlacesTemplateProps = {
 }
 
 export default function PlacesTemplate({ place }: PlacesTemplateProps) {
+  const router = useRouter()
+  if (router.isFallback) return null
   return (
     <>
       <LinkWrapper href="/">
         <CloseOutline size={32} aria-label="Go back to map" />
       </LinkWrapper>
-      <h1>{place.name}</h1>
-      <div dangerouslySetInnerHTML={{ __html: place.description.html }} />
-      {place.gallery.map((image, index) => (
-        <Image
-          key={`photo-${index}`}
-          src={image.url}
-          alt={place.name}
-          width={image.width}
-          height={image.height}
-          quality={75}
-        />
-      ))}
+      <S.Wrapper>
+        <S.Container>
+          <S.Heading>
+            <h1>{place.name}</h1>
+          </S.Heading>
+          <S.Body>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: place.description?.html || ''
+              }}
+            />
+          </S.Body>
+          <S.Gallery>
+            {place.gallery.map((image, index) => (
+              <Image
+                key={`photo-${index}`}
+                src={image.url}
+                alt={place.name}
+                width={image.width}
+                height={image.height}
+                quality={75}
+              />
+            ))}
+          </S.Gallery>
+        </S.Container>
+      </S.Wrapper>
     </>
   )
 }
